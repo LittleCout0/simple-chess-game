39123bc8d23be7e6d4cdee5e0455ba5abb20ebad
William Coutinho <william.csouza@hotmail.com>
2023-07-17 00:44:01 -0300

    Added Queen piece + bug fix (checkmate)

--- src/application/Program.java
+++ src/application/Program.java
@@ -36,7 +36,8 @@ public class Program {
                 if(capturedPiece != null){
                     captured.add(capturedPiece);
                 }
-            }catch (ChessException | InputMismatchException e){
+            }
+            catch (ChessException | InputMismatchException e){
                 System.out.println(e.getMessage());
                 sc.nextLine();
             }
